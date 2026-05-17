616820ef0dd1cc2bf26f94a57ca4034654591010
somil4632 <somil0999@gmail.com>
2026-05-17 14:54:02 +0530

    worked on styling the navbar and add home , about us , contact us section

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -4,20 +4,21 @@ import logo from "../../assets/freshcart logo.png"
 import cart from "../../assets/cart.png"
 const Navbar = () => {
     return (
-        <div class='logo'>
-            <div class="text">
-                <img src={logo} alt="freshcart logo" height="200px"/>
+        <div className='navbar'>
+            <div className="nav-logo">
+                <img src={logo} alt="freshcart logo" height="80px" width="160px" />
                 <p>From Farm To Your Door</p>
             </div>
-            <ul class="Grocery">
+            <ul className="nav-menu">
               <li>Home</li>
-                <li>About Us</li>
-                <li>Contact US</li>
-                <li>Categories</li>
+              <li>About Us</li>
+              <li>Contact US</li>
+              <li>Categories</li>
             </ul>
-            <div class="login-cart">
+            <div className="nav-login-cart">
                 <button>Login</button>
                 <img src={cart} alt="" height="50px" />
+                <div className="nav-cart-count">0</div>
             </div>
         </div>
     );
