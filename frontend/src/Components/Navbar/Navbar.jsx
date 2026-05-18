2590b13446978c704f026d07cab23e6a37403ed4
somil4632 <somil0999@gmail.com>
2026-05-18 16:58:20 +0530

    added the underlined hover into drinks and categories navigation menu

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -13,10 +13,10 @@ const Navbar = () => {
             </div>
             <ul className="nav-menu">
               <li onClick={() => setMenu("home")}><Link to="/">Home</Link>{menu==="home" ? <hr/>:<></>}</li>
-              <li onClick={() => setMenu("grocery")}><Link to="/">Grocery</Link>{menu==="grocery" ? <hr/>:<></>}</li>
-              <li onClick={() => setMenu("snacks")}><Link to="/">Snacks</Link> {menu==="snacks" ? <hr/>:<></>}</li>
-              <li onClick={() => setMenu("drinks ")}><Link to="/">Drinks</Link> {menu==="drinks" ? <hr/>:<></>}</li>
-               <li onClick={() => setMenu("categories ")}><Link to="/">categories</Link> {menu==="categories" ? <hr/>:<></>}</li>
+              <li onClick={() => setMenu("grocery")}><Link to="grocery">Grocery</Link>{menu==="grocery" ? <hr/>:<></>}</li>
+              <li onClick={() => setMenu("snacks")}><Link to="/snacks">Snacks</Link> {menu==="snacks" ? <hr/>:<></>}</li>
+              <li onClick={() => setMenu("drinks")}><Link to="/drinks">Drinks</Link> {menu==="drinks" ? <hr/>:<></>}</li>
+               <li onClick={() => setMenu("categories")}><Link to="/categories">categories</Link> {menu==="categories" ? <hr/>:<></>}</li>
             </ul>
             <div className="nav-login-cart">
                 <button>Login</button>
