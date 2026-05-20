dad1c5cfa6a7bb310bcff59003b9f277a6bad356
somil4632 <somil0999@gmail.com>
2026-05-20 13:41:36 +0530

    update hero Section

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import "./Hero.css"
 import handicon from "../../assets/hand-icon.png"
+import arrowicon from "../../assets/arrow icon.webp"
+import mango from "../../assets/mango.webp"
 const Hero = () => {
     return (
         <div className="hero">
             <div className=" hero-left">
-               <div className=" hand-hand-icon">
-                <img src="" alt=""/>
-            <p>new</p>
-            <p>collections</p>
-            <p>for everyone</p>
-         </div>
-
-         <div>
-            <img src={handicon} alt="" height=" 80px" />
-              <h2>Best Deals ! Best Prices !</h2>
-        </div>
-       
-            <div className=" hero-right">
-        </div>
-        </div>
+                    <h2>Best Deals ! Best Prices !</h2>
+                  <div className=" hand-hand-icon">
+                   <p>new</p>
+                      <img src={handicon} alt="" height=" 80px" />
+                      </div>
+                         <p>collections</p>
+                         <p>for everyone</p>
+                            </div>
+                            <div className=" hero-latest-btn">
+                                <div>Latest Collection</div>
+                                 <img src={arrowicon} alt="" height="40px"/>
+                            </div>
+                       <div className=" hero-right">
+                        <img src={mango} alt="" height="160px"/>
+               </div>
         </div>
     )
 }
