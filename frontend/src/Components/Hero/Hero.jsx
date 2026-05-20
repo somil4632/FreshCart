1da1946732a91bcc2efaa95ee27f033072c9002d
somil4632 <somil0999@gmail.com>
2026-05-20 15:29:50 +0530

    edited Hero Section Styling and added the grocery items all together image

--- frontend/src/Components/Hero/Hero.jsx
+++ frontend/src/Components/Hero/Hero.jsx
@@ -2,25 +2,26 @@ import React from 'react'
 import "./Hero.css"
 import handicon from "../../assets/hand-icon.png"
 import arrowicon from "../../assets/arrow icon.webp"
-import mango from "../../assets/mango.webp"
+import groceryitems from "../../assets/grocery items.png"
 const Hero = () => {
     return (
         <div className="hero">
             <div className=" hero-left">
                     <h2>Best Deals ! Best Prices !</h2>
-                  <div className=" hand-hand-icon">
+                  <div className=" hero-hand-icon">
                    <p>new</p>
-                      <img src={handicon} alt="" height=" 80px" />
+                      <img src={handicon} alt="" height="70px" />
                       </div>
                          <p>collections</p>
                          <p>for everyone</p>
-                            </div>
-                            <div className=" hero-latest-btn">
+                           <div className=" hero-latest-btn">
                                 <div>Latest Collection</div>
                                  <img src={arrowicon} alt="" height="40px"/>
                             </div>
+                            </div>
+                          
                        <div className=" hero-right">
-                        <img src={mango} alt="" height="160px"/>
+                        <img src={groceryitems} alt="" height="300px"/>
                </div>
         </div>
     )
