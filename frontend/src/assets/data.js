2bab70a57f1c0824b9389e57fe7da7ffd0bf607e
somil4632 <somil0999@gmail.com>
2026-05-23 17:26:24 +0530

    Added new grocery product images

--- frontend/src/assets/data.js
+++ frontend/src/assets/data.js
@@ -2,6 +2,31 @@ import p1_img from '../assets/thumbs up.png'
 import p2_img from '../assets/Atta.png'
 import p3_img from '../assets/yellow lays.png'
 import p4_img from '../assets/garlic powder.png'
+import p5_img from '../assets/amul gold.png'
+import p6_img from '../assets/bonn white bread.png'
+import p7_img from '../assets/britinina bread.png'
+import p8_img from '../assets/bonn bombay pav.png'
+import p9_img from '../assets/chocos.png'
+import p10_img from '../assets/amul moti tonned.png'
+import p11_img from '../assets/yakuit.webp'
+import p12_img from '../assets/alpino oats.png'
+import p13_img from '../assets/eggoz.png'
+import p14_img from '../assets/farm made eggs.png'
+import p15_img from '../assets/corn flakes.png'
+import p16_img from '../assets/nestle milk.webp'
+import p17_img from '../assets/yoga bar dark oats.webp'
+import p18_img from '../assets/aanand panner.png'
+import p19_img from '../assets/amul masti curd.png'
+import p20_img from '../assets/amul butter.webp'
+import p21_img from '../assets/dlecta cheese.webp'
+import p22_img from '../assets/dlecta cream.webp'
+import p23_img from '../assets/thai rice.webp'
+import p24_img from '../assets/wheat noodles.webp'
+import p25_img from '../assets/peanut butter.png'
+import p26_img from '../assets/RiteBite protien bar.webp'
+import p27_img from '../assets/cucumber.png'
+import p28_img from '../assets/Bobby Muskmelon.png'
+
 
 let data_product = [
     {
@@ -25,8 +50,8 @@ let data_product = [
         name: "Yellow lays",
         category: "Snacks & Munches",
         image: p3_img,
-        new_price: 20,
-        old_price: 10,
+        new_price: 15,
+        old_price: 20,
     },
     {
         id: 4,
@@ -36,6 +61,39 @@ let data_product = [
         new_price: 260,
         old_price: 300,
     },
+      {
+            id: 5,
+            name: "Amul Gold Milk",
+            category: "Dairy Products",
+            image: p5_img,
+            new_price: 65,
+            old_price: 70,
+        },
+    
+        {id:6, name: "Bonn White Bread" , category: "Dairy Product" , image: p6_img , new_price: 45, old_price: 60  },
+        {id:7, name: "britinina bread", category: "Dairy Product", image: p7_img, new_price: 20, old_price: 22 },
+        {id:8, name: "Bombay Pav", category: "Dairy Product", image: p8_img, new_price: 40, old_price: 35 },
+        {id:9, name: "Chocos", category: "Dairy Product", image: p9_img, new_price: 200, old_price: 180 },
+        {id:10, name: "Amul Moti Tonned Milk", category: "Dairy Product", image: p10_img, new_price: 72, old_price: 80 },
+        {id:11, name: "Yakiut", category: "Dairy Product", image: p11_img, new_price: 30, old_price: 35 },
+        {id:12, name: "Alpino Oats", category: "Dairy Product & Breakfast", image: p12_img, new_price: 90, old_price: 120 },
+        {id:13, name: "Eggoz", category: "Dairy Product & Breakfast", image: p13_img, new_price: 180, old_price: 150 },
+        {id:14, name: "Farm Made Egg", category: "Dairy Product & Breakfast", image: p14_img, new_price: 420, old_price: 380 },
+        {id:15, name: "Corn Flakes", category: "Dairy Product & Breakfast", image: p15_img, new_price: 280, old_price: 200 },
+        {id:16, name: "Nestle Milk", category: "Dairy Product", image: p16_img, new_price: 190, old_price: 166 },
+        {id:17, name: "Yoga Bar Dark Oats", category: "Dairy Product & Breakfast", image: p17_img, new_price: 150, old_price: 110 },
+        {id:18, name: "Aanand Panner", category: "Dairy Product", image: p18_img, new_price: 300, old_price: 250 },
+        {id:19, name: "Amul Masti Curd", category: "Dairy Product", image: p19_img, new_price: 80, old_price: 70 },
+        {id:20, name: "Amul Butter", category: "Dairy Product & Breakfast", image: p20_img, new_price: 120, old_price: 100 },
+        {id:21, name: "Dlecta cheese", category: "Dairy Product", image: p21_img, new_price: 180, old_price: 150 },
+        {id:22, name: "Dlecta Cream", category: "Dairy Product", image: p22_img, new_price: 220, old_price: 1902 },
+        {id:23, name: "Thai Rice", category: "Breakfast", image: p23_img, new_price: 80, old_price: 62 },
+        {id:24, name: "Wheat Noodles", category: "Breakfast", image: p24_img, new_price: 100, old_price: 90 },
+        {id:25, name: "Peanut Butter", category: "Breakfast", image: p25_img, new_price: 180, old_price: 160 },
+        {id:26, name: "RiteBite Bar", category: "Dairy Product & Breakfast", image: p26_img, new_price: 50, old_price: 45 },
+        {id:27, name: "Cucumber", category: "Vegetable ", image: p27_img, new_price: 40, old_price: 60 },
+        {id:28, name: "Bobby Muskmelon", category: "Fruits ", image: p28_img, new_price: 40, old_price: 50 },
+    
 ]
 
 export default data_product;
