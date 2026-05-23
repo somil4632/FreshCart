5d33be6b9180d2037e51f20b4ddf60bebcd62c19
somil4632 <somil0999@gmail.com>
2026-05-23 12:17:30 +0530

    Created Homepage featured grocery products section.

---
+++ frontend/src/assets/data.js
@@ -0,0 +1,41 @@
+import p1_img from '../assets/Thumbs uo.png'
+import p2_img from '../assets/atta.png'
+import p3_img from '../assets/blue lays.webp'
+import p4_img from '../assets/cardamom powder.webp'
+
+let data_product = [
+    {
+        id: 1,
+        name: "Thumbs Up",
+        category: "Drinks",
+        image: p1_img,
+        new_price: 40,
+        old_price: 45,
+    },
+    {
+        id: 2,
+        naem:"Multigrain Atta",
+        category: "Wheat",
+        image: p2_img,
+        new_price: 350,
+        old_price: 370,
+    },
+    {
+        id: 3,
+        name: "Blue Lays",
+        category: "Snacks & Munches",
+        image: p3_img,
+        new_price: 20,
+        old_price: 10,
+    },
+    {
+        id: 4,
+        naem: "Cardamom Powder",
+        category: "Masala & Oil",
+        image: p4_img,
+        enw_price: 260,
+        old_price: 300,
+    },
+]
+
+export default data_product;
