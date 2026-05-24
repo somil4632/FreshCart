a8b22ce5382d6680bad4a04e43f000b8a7e04c50
somil4632 <somil0999@gmail.com>
2026-05-24 12:03:11 +0530

    Update Popular styling Secton , added offers section and improved homepage layout

---
+++ frontend/src/Components/Offers/Offers.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Offers.css"
+import Monster from "../../assets/Monster.png"
+const Offers = () => {
+    return (
+        <div className='offers'>
+            <div className="offers-left">
+                <h1>Exclusive</h1>
+                <h1>Offers For You</h1>
+                <p>ONLY ON BEST SELLER PRODUCTS</p>
+                <button>Check Now</button>
+            </div>
+            <div className="offers-right">
+               <img src={Monster} alt=""/>
+            </div>
+        </div>
+    )
+}
+export default Offers
