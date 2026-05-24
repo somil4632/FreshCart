884315fc057b6ffc2fc55cdbe2e4b1163f368657
somil4632 <somil0999@gmail.com>
2026-05-24 19:54:04 +0530

    Added NewCollection component with product cards and imported new collection product images for the homepage

--- frontend/src/Pages/Shop.jsx
+++ frontend/src/Pages/Shop.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
+import Newcollection from '../Components/NewCollection/NewCollection'
 const Shop = () => {
     return (
         <div>
             <Hero/>
             <Popular/>
             <Offers/>
+            <NewCollection/>
         </div>
     )
 }
---
+++ frontend/src/assets/newcollection.js
@@ -0,0 +1,11 @@
+import p28_img from '../assets/boon pizza base.png'
+import p29_img from '../assets/mango.png'
+import p30_img from '../assets/chcoco chip muffin.png'
+
+
+let new_collection = [
+    { id: 28, name: "Bonn Pizza Base", category: "Bread", image: p28_img, new_price: 50, old_price: 80},
+    { id: 29, name: "Pure Mango", category: "Fruits", image: p29_img, new_price: 120, old_price: 150},
+    { id: 30, name: "Choco Chip Muffin", category: "Sweet Tooth", image: p30_img, new_price: 180, old_price: 220},
+]
+export default new_collection;
