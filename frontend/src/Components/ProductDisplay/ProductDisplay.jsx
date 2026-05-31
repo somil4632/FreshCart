e67c8a0ac1002ab9c04a4ed91893959d30701ac3
somil4632 <somil0999@gmail.com>
2026-05-31 17:47:21 +0530

    Enhanced ProductDisplay page by adding product images , ratings , pricing , description , quantity selection , category detalis and add to cart button

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./ProductDisplay.css"
+import staricon from "../../assets/star icon.png"
 const ProductDisplay = (props) => {
     const {product} = props;
     if(!product){
@@ -9,12 +10,50 @@ const ProductDisplay = (props) => {
         <div className='productdisplay'>
           <div className="productdisplay-left">
             <div className="productdisplay-img-list">
-                <img src={product.image} alt=""/>
-                 <img src={product.image} alt=""/>
+                <img src={product.image} alt="" height="200px"/>
+                 <img src={product.image} alt=""height="200px"/>
+            </div>
+            <div className='productdisplay-img'>
+                <img className='productdisplay-main-img' src={product.image} alt="" height="500px"/>
             </div>
           </div>
-          <div className="productdisplay-right">
 
+          <div className="productdisplay-right">
+            <h1>{product.name}</h1>
+            <div className="productdisplay-right-star">
+                <img src={staricon} alt="" height="20px" />
+                 <img src={staricon} alt="" height="20px" />
+                  <img src={staricon} alt="" height="20px" />
+                   <img src={staricon} alt="" height="20px" />
+                    <img src={staricon} alt="" height="20px" />
+                    <p>{130}</p>
+            </div>
+             <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">
+                     &#8377;{product.old_price}
+                </div>
+                 <div className="productdisplay-right-price-new">
+                     &#8377;{product.new_price}
+                 </div>
+                 <div className="productdisplay-right-description">
+                    Fresh and high-quality grocery products available at affordable prices for everyday shopping needs
+                 </div>
+                 <div className='productdisplay-right-quantity'>
+                    <h1>Select Quantity</h1>
+                    <div className="productdisplay-right-quantity">
+                        <div>1 Pack</div>
+                        <div>2 Pack</div>
+                        <div>3 Pack</div>
+                    </div>
+                 </div>
+                 <button>ADD TO CART</button>
+                 <div className="productdisplay-right-category">
+                    <span>Category:<span> Snacks , Drinks , Vegetables , Dairy Products</span></span>
+                 </div>
+                 <div className="productdisplay-right-category">
+                    <span>Tags:<span> Fresh , Oragnic , Healthy , Best Quality</span></span>
+                 </div>
+             </div>
           </div>
         </div>
     )
