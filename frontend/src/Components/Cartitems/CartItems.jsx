71c3aa61092710a555be6233f32068bca2533968
somil4632 <somil0999@gmail.com>
2026-06-05 23:41:40 +0530

    Added CartItems functionality, improved cart layout using CSS.

--- frontend/src/Components/Cartitems/CartItems.jsx
+++ frontend/src/Components/Cartitems/CartItems.jsx
@@ -1,5 +1,6 @@
 import React, {useContext} from 'react'
 import "./CartItems.css"
+import removeicon from "../../assets/remove icon.png"
 import {ShopContext} from "../../Context/ShopContext";
 const CartItems = () => {
     const {all_products,cartItems,removeFromCart} = useContext(ShopContext);
@@ -13,7 +14,45 @@ const CartItems = () => {
                 <p>Total</p>
                 <p>Remove</p>
             </div>
+            <hr/>
+           {all_products.map((e)=>{
+            if(cartItems[e.id]>0)
+            {
+                return  (
+                 <div key={e.id}>
+                   <div className="cartitems-format">
+                    <img src={e.image} alt="" className='carticon-product-icon' height="100px"/>
+                    <p>{e.name}</p>
+                    <p>&#8377;{e.new_price}</p>
+                    <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+                    <p>{e.new_price*cartItems[e.id]}</p>
+                    <img src={removeicon} alt="" onClick={()=> removeFromCart(e.id)} height="40px"/>
+                </div>
+                <hr/>
+            </div>
+                );
+            }
+             return null;
+           })}
+        
+        <div className="cartitems-down">
+           <div className="cartitems-total">
+            <h1>Cart Totals </h1>
+             <div>
+                <div className="cartitems-total-item">
+                    <p>Subtotal</p>
+                    <p>&#8377;{0}</p>
+                </div>
+                <hr/>
+                 <div className="cartitems-total-item">
+                    <p>Shipping Fee</p>
+                    <p>&#8377;{0}</p>
+                </div>
+                <hr/>
+             </div>
+           </div>
         </div>
-    )
-}
+    </div>
+    );
+};
 export default CartItems
