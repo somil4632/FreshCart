a2dac7530405266f6c9d44047f281392d28f7dd6
somil4632 <somil0999@gmail.com>
2026-06-08 14:22:52 +0530

    same for CartItems

--- frontend/src/Pages/Cart.jsx
+++ frontend/src/Pages/Cart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CartItems from "../Components/CartItems/CartItems";
+import CartItems from "../Components/Temp/Cartitems";
 const Cart = () => {
     return (
         <div>
